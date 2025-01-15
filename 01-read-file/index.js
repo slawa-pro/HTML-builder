db7a98395635639a9ca2a91c862660339bea26b4
Vyacheslav <va.povhleb@gmail.com>
2025-01-15 17:47:47 +0300

    init: start task HTML-BUILDER

---
+++ 01-read-file/index.js
@@ -0,0 +1,15 @@
+//https://nodejs.org/api/stream.html#readable-streams
+
+const path = require('path');
+const fs = require('fs');
+
+function f01() {
+  const pathToFile = path.join(__dirname, 'text.txt');
+  const readStream = fs.createReadStream(pathToFile, 'utf8');
+
+  readStream.on('data', function (chunk){
+    console.log(chunk);
+  });
+}
+
+f01();
